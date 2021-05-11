6256a8e0fefd13aea1445707d608b26011366c60
DELL <DELL@DESKTOP-AA0KSH6>
2021-05-11 10:56:33 +0530

    Loops and Arrays practise

---
+++ class3/src/class3/Arrays9.java
@@ -0,0 +1,20 @@
+package class3;
+
+import java.util.*;
+public class Arrays9 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number of elements ");
+    int n = sc.nextInt();
+    int[] a =new int[n];
+    int i,j;
+    for(i=0;i<n;i++) 
+    {
+      a[i]=sc.nextInt();
+    }
+  }
+
+
+
+}
