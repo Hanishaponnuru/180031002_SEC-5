850001cc8bf2dca0538f93a68709457a928b1fa5
DELL <DELL@DESKTOP-AA0KSH6>
2021-05-11 11:27:56 +0530

    Loops and Arrays practise

---
+++ class3/src/class3/problem3.java
@@ -0,0 +1,18 @@
+package class3;
+
+import java.util.*;
+public class problem3 {
+
+
+	public static void main(String[] args) {
+		if (args.length ==0)
+        {
+        System.out.println("No Values");
+        }
+    else{
+        for(String i : args)
+            System.out.print(i+" , ");   
+        }
+	}
+
+}
